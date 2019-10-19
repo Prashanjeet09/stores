15a0e91f9e434e8e364b17295031d849d448f948
Prashanjeet09 <50791785+Prashanjeet09@users.noreply.github.com>
2019-10-19 15:04:42 +0530

    g

---
+++ src/data.js
@@ -0,0 +1,119 @@
+export const storeProducts = [
+    {
+        id:1,
+        title: "Google Pixel- Black",
+        img: "img/product-1.png",
+        price: 10,
+        company: "GOOGLE",
+        info:
+           "It sports a 8-megapixel camera on the front for selfies. Google Pixel based on Android 7.1 and packs 32GB of inbuilt storage. The Google Pixel is a single SIM (GSM) smartphone that accepts a Nano-SIM card. Connectivity options on the Google Pixel include Wi-Fi 802.11 a/b/g/n/ac, GPS, Bluetooth v4.",
+        inCart: false,
+        count: 0,
+        toal:0
+
+    },
+
+    {
+        id:2,
+        title: "Samsung S7",
+        img: "img/product-2.png",
+        price: 16,
+        company: "SAMSUNG",
+        info:
+           "It sports a 8-megapixel camera on the front for selfies. Google Pixel based on Android 7.1 and packs 32GB of inbuilt storage. The Google Pixel is a single SIM (GSM) smartphone that accepts a Nano-SIM card. Connectivity options on the Google Pixel include Wi-Fi 802.11 a/b/g/n/ac, GPS, Bluetooth v4.",
+        inCart: false,
+        count: 0,
+        toal:0
+    },
+
+    {
+        id:3,
+        title: "HTC 10- Black",
+        img: "img/product-3.png",
+        price: 8,
+        company: "HTC",
+        info:
+           "It sports a 8-megapixel camera on the front for selfies. Google Pixel based on Android 7.1 and packs 32GB of inbuilt storage. The Google Pixel is a single SIM (GSM) smartphone that accepts a Nano-SIM card. Connectivity options on the Google Pixel include Wi-Fi 802.11 a/b/g/n/ac, GPS, Bluetooth v4.",
+        inCart: false,
+        count: 0,
+        toal:0
+    },
+
+    {
+        id:4,
+        title: "Oppo-reno",
+        img: "img/product-1.png",
+        price: 18,
+        company: "OPPO",
+        info:
+           "It sports a 8-megapixel camera on the front for selfies. Google Pixel based on Android 7.1 and packs 32GB of inbuilt storage. The Google Pixel is a single SIM (GSM) smartphone that accepts a Nano-SIM card. Connectivity options on the Google Pixel include Wi-Fi 802.11 a/b/g/n/ac, GPS, Bluetooth v4.",
+        inCart: false,
+        count: 0,
+        toal:0
+    },
+
+    {
+        id:5,
+        title: "Redmi-note7 pro",
+        img: "img/product-1.png",
+        price: 14,
+        company: "XIOMI",
+        info:
+           "It sports a 8-megapixel camera on the front for selfies. Google Pixel based on Android 7.1 and packs 32GB of inbuilt storage. The Google Pixel is a single SIM (GSM) smartphone that accepts a Nano-SIM card. Connectivity options on the Google Pixel include Wi-Fi 802.11 a/b/g/n/ac, GPS, Bluetooth v4.",
+        inCart: false,
+        count: 0,
+        toal:0
+    },
+
+    {
+        id:6,
+        title: "Vivo-v10",
+        img: "img/product-1.png",
+        price: 10,
+        company: "VIVO",
+        info:
+           "It sports a 8-megapixel camera on the front for selfies. Google Pixel based on Android 7.1 and packs 32GB of inbuilt storage. The Google Pixel is a single SIM (GSM) smartphone that accepts a Nano-SIM card. Connectivity options on the Google Pixel include Wi-Fi 802.11 a/b/g/n/ac, GPS, Bluetooth v4.",
+        inCart: false,
+        count: 0,
+        toal:0
+    },
+
+    {
+        id:7,
+        title: "Nokia-8",
+        img: "img/product-1.png",
+        price: 16,
+        company: "NOKIA",
+        info:
+           "It sports a 8-megapixel camera on the front for selfies. Google Pixel based on Android 7.1 and packs 32GB of inbuilt storage. The Google Pixel is a single SIM (GSM) smartphone that accepts a Nano-SIM card. Connectivity options on the Google Pixel include Wi-Fi 802.11 a/b/g/n/ac, GPS, Bluetooth v4.",
+        inCart: false,
+        count: 0,
+        toal:0
+    },
+
+    {
+        id:8,
+        title: "HTC-desire",
+        img: "img/product-1.png",
+        price: 10,
+        company: "HTC",
+        info:
+           "It sports a 8-megapixel camera on the front for selfies. Google Pixel based on Android 7.1 and packs 32GB of inbuilt storage. The Google Pixel is a single SIM (GSM) smartphone that accepts a Nano-SIM card. Connectivity options on the Google Pixel include Wi-Fi 802.11 a/b/g/n/ac, GPS, Bluetooth v4.",
+        inCart: false,
+        count: 0,
+        toal:0
+    }
+];
+
+export const detailProduct ={
+    id:1,
+    title: "Google Pixel- lack",
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info:
+       "It sports a 8-megapixel camera on the front for selfies. Google Pixel based on Android 7.1 and packs 32GB of inbuilt storage. The Google Pixel is a single SIM (GSM) smartphone that accepts a Nano-SIM card. Connectivity options on the Google Pixel include Wi-Fi 802.11 a/b/g/n/ac, GPS, Bluetooth v4.",
+    inCart: false,
+    count: 0,
+    toal:0
+};
